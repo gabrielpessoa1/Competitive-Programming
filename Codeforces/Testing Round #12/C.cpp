0f168c4081f54882c453a0ce2a9dbd3f012ebb1a
gabrielpessoa1 <gap@cin.ufpe.br>
2019-05-07 00:58:36 -0300

    Testing Round #12/C.cpp fixed

--- Codeforces/Testing Round #12/C.cpp
+++ Codeforces/Testing Round #12/C.cpp
@@ -7,7 +7,7 @@ int n, k;
 long long bit[15][ms];
 int arr[ms];
 
-void update(int i, int idx, int v) {
+void update(int i, int idx, long long v) {
   while(idx <= n) {
     bit[i][idx] += v;
     idx += idx & -idx;
@@ -26,13 +26,14 @@ long long query(int i, int idx) {
 int main() {
   cin >> n >> k;
   long long ans = 0;
-  update(0, 1, 1);
+  if(k == 0) ans = 1;
   for(int i = 0; i < n; i++) {
     cin >> arr[i];
-    ans += query(k, arr[i]);
-    for(int j = k; j > 0; j--) {
-      update(j, arr[i], query(j-1, arr[i]));
+    update(1, arr[i], 1);
+    for(int j = 2; j <= k; j++) {
+      update(j, arr[i], query(j-1, arr[i]-1));
     }
+    ans += query(k, arr[i]-1);
   }
   cout << ans << '\n';
 }

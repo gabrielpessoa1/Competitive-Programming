fbdac92f27cbaa3e799c16bbdaffe2c6f44c515e
gabrielpessoa1 <gap@cin.ufpe.br>
2019-05-19 12:44:04 -0300

    Educational Codeforces #65 - solved during contest

---
+++ Educational Codeforces #65/A.cpp
@@ -0,0 +1,23 @@
+// Educational Codeforces Round #65
+// A - Telephone Number
+// https://codeforces.com/contest/1167/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    string s;
+    int n;
+    cin >> n;
+    cin >> s;
+    bool valid = false;
+    for(int i = 0; i < (int) (s.size()) - 10; i++) {
+      if(s[i] == '8') valid = true;
+    }
+    if(valid) cout << "YES\n";
+    else cout << "NO\n";
+  }
+}
---
+++ Educational Codeforces #65/D.cpp
@@ -0,0 +1,27 @@
+// Educational Codeforces Round #65
+// D - Bicolored RBS
+// https://codeforces.com/contest/1167/problem/D
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int ms = 5e5+5;
+
+int main() {
+  cin.tie(0); ios::sync_with_stdio(0);
+  int n;
+  string s;
+  cin >> n >> s;
+  int arr[2] = {0, 0};
+  for(int i = 0; i < n; i++) {
+    int greater = arr[1] > arr[0];
+    if(s[i] == ')') {
+      arr[greater]--;
+      cout << greater;
+    } else {
+      arr[!greater]++;
+      cout << !greater;
+    }
+  }
+  cout << endl;
+}
